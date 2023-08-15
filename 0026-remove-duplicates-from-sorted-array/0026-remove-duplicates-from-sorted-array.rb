f11f8a5603616543cfc25d992f82bf6e8d67fa71
Mohamed Magdy <mohamedmagdysaber@gmail.com>
2023-08-15 15:13:37 +0300

    Time: 62 ms (97.19%), Space: 211.8 MB (84.14%) - LeetHub

---
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.rb
@@ -0,0 +1,16 @@
+# @param {Integer[]} nums
+# @return {Integer}
+def remove_duplicates(nums)
+    return 0 if nums.empty?
+    
+    k = 1
+    
+    (1...nums.length).each do |i|
+        if nums[i] != nums[i - 1]
+            nums[k] = nums[i]
+            k += 1
+        end
+    end
+    
+    k
+end
